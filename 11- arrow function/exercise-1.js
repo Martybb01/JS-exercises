91ca1496b4bab26712eb492e885fe9055e26bbb4
Martib01 <martybb01@gmail.com>
2022-05-22 18:47:02 +0200

    transform sum and log functions

---
+++ 11- arrow function/exercise-1.js
@@ -0,0 +1,17 @@
+const summa = function (a, b) {
+  return a + b;
+};
+
+const logga = function (value) {
+  console.log(value);
+};
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const log = (value) => {
+  console.log(value);
+};
+
+log(sum(2, 5));
