db5c58057f988d21878f4729ce8cb76a0571babd
Martib01 <martybb01@gmail.com>
2022-05-29 19:01:39 +0200

    keys() and entries()

---
+++ 23- object methods/exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+// Print values of person using Object.keys
+console.log(Object.keys(person));
+
+// Print values of person using Object.entries
+console.log(Object.entries(person));
