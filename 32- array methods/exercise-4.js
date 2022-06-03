375fef6d425f10c2d39f271537bb81a75bfa6c3f
Martib01 <martybb01@gmail.com>
2022-06-03 18:59:18 +0200

    for loop

---
+++ 32- array methods/exercise-4.js
@@ -0,0 +1,77 @@
+// function uncompletedNotes(notes) {
+//   return notes.map(function (note) {
+//     if (notes[0].todos[0].done === false) {
+//       return "Note: " + note.description + " is not done yet.";
+//     } else {
+//       return "Note: " + note.description + " is done.";
+//     }
+//   });
+// }
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: true,
+      },
+    ],
+  },
+];
+
+function uncompletedNotes(notes) {
+  for (let i = 0; i < notes.length; i++) {
+    for (let j = 0; j < notes[i].todos.length; j++) {
+      if (notes[i].todos[j].done === false) {
+        console.log("Note: " + notes[i].description + " is not done yet.");
+        return notes[i]; // return the note object
+        // return notes[i].description; // return only the note description (es. Workout program)
+      }
+    }
+  }
+}
+
+const notesInProgress = uncompletedNotes(notes);
+console.log("All notes: ", notes);
+console.log("Notes In Progress: ", notesInProgress);
+
+console.log(notes[0].todos[0].done); // log di prova
