0a2c45a2f86489517a4c07c55a263b96b3996816
Martib01 <martybb01@gmail.com>
2022-06-07 10:21:24 +0200

    class extend with ternary condition

---
+++ 38- le classi e l'ereditarietà/exercise-5.js
@@ -0,0 +1,40 @@
+class BankAccount {
+  #amount = 0;
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+  deposit(amount) {
+    this.#amount += amount;
+  }
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class BankAccountVip extends BankAccount {
+  constructor(initialAmount) {
+    super(initialAmount);
+  }
+  // deposit(amount) {
+  //   if (amount >= 1000) {
+  //     super.deposit(amount + (amount / 100) * 3);
+  //   } else {
+  //     super.deposit(amount);
+  //   }
+  deposit(amount) {
+    amount =
+      amount >= "1000"
+        ? super.deposit(amount + (amount / 100) * 3)
+        : super.deposit(amount);
+  }
+}
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view(); // 5541
