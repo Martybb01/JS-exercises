59ef5af1e50a8a900eab83d79e76cc0a8ba78909
Martib01 <martybb01@gmail.com>
2022-06-07 10:10:22 +0200

    class and methods

---
+++ 37- le classi/exercise-4.js
@@ -0,0 +1,23 @@
+class BankAccount {
+  balance;
+  constructor(balance) {
+    this.balance = balance;
+  }
+  deposit(amount) {
+    this.balance += amount;
+    return this.balance;
+  }
+  withdraw(amount) {
+    this.balance -= amount;
+    return this.balance;
+  }
+  view() {
+    console.log(`your balance is ${this.balance}`);
+  }
+}
+
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(800);
+bankAccount.view(); // your balance is 900
