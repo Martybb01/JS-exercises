d1e90e7aa19a88296e112cbd322ca29f42d3361f
Martib01 <martybb01@gmail.com>
2022-06-08 10:33:56 +0200

    fix log(prop) + add new way to resolve exercise

--- 09- blocchi condizionali e cicli/exercise-6.js
+++ 09- blocchi condizionali e cicli/exercise-6.js
@@ -1,9 +1,3 @@
-function getKeys(obj) {
-  for (let prop in obj) {
-    console.log(`${prop}`);
-  }
-}
-
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -12,5 +6,24 @@ const person = {
   job: "Developer",
 };
 
+// METODO 1
+function getKeys(obj) {
+  for (let prop in obj) {
+    console.log(prop);
+  }
+} // stampa le chiavi a elenco, una sotto l'altra
+
 const keys = getKeys(person);
 console.log(keys);
+
+//METODO 2
+function getKeys2(obj) {
+  let objectPerson = [];
+  for (let prop in obj) {
+    objectPerson.push(prop);
+  }
+  return objectPerson;
+} // stampa le chiavi in un nuovo array
+
+const keys2 = getKeys2(person);
+console.log(keys2);
