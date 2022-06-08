56880a0819811dc546931e418d5de623fcd315b4
Martib01 <martybb01@gmail.com>
2022-06-08 19:12:51 +0200

    fix

--- 18- closures e concatenazione/exercise-5.js
+++ 18- closures e concatenazione/exercise-5.js
@@ -1,12 +1,28 @@
 function memoize(fn) {
-  let chace = {};
-  return function addCache(num) {
-    if (num in chace) {
-      return `Fetching from chace for ${chace[num]}`;
+  let cache = {};
+  let refNum = null; // lo inizializza ogni volta
+
+  return (num) => {
+    if (refNum == null) {
+      refNum = num; // se ancora non è stato impostato siamo nel primo giro, quindi refnum = numero dell'utente
+    }
+
+    if (num in cache) {
+      console.log(num, refNum);
+      if (num == refNum) {
+        // allora siamo all'ultimo giro e quindi ho già il risultato
+        refNum = null; // lo reimposto così prossima volta che la funzione viene chiamata sarà di nuovo null
+        return `Fetching from cache for ${cache[num]}`;
+      }
+      return cache[num]; // restituisco il numero perchè non è l'ultimo giro
     } else {
-      chace[num] = fn(num);
-      return chace[num];
-      // return `Calculating result for ${chace[num]}` non funziona nel mio caso non so perchè
+      cache[num] = fn(num);
+      console.log(num, refNum);
+      if (num == refNum) {
+        refNum = null;
+        return `Calculating from cache for ${cache[num]}`;
+      }
+      return cache[num];
     }
   };
 }
@@ -15,10 +31,17 @@ function factorial(num) {
   if (num === 0) {
     return 1;
   }
-  return num * factorial(num - 1);
+
+  return num * factorial(num - 1, num == 1);
 }
 
 factorial = memoize(factorial);
-console.log(factorial(10)); // 3628800
-console.log(factorial(6)); // Fetching from chace for 720
-console.log(factorial(5)); // Fetching from chace for 120
+console.log(factorial(10)); // 10 è il numero passato dell'utente
+console.log(factorial(6));
+console.log(factorial(5));
+console.log(factorial(10));
+console.log(factorial(12));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(12));
+console.log(factorial(13));
