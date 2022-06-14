f8ea5f5cd67a1cf9cf497f1c03b18417c2d729a7
Martib01 <martybb01@gmail.com>
2022-06-14 10:47:18 +0200

    rest operator

---
+++ 53- Rest parameters and spread syntax/exercise-1.js
@@ -0,0 +1,16 @@
+// METODO 1
+function sum1(...nums) {
+  return nums.reduce((acc, num) => acc + num, 0);
+}
+console.log(sum1(1, 2, 3, 4, 5)); // 15
+
+// METODO 2
+function sum2(...nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+console.log(sum2(1, 2, 3, 4, 5, 20, 40, 55)); // 130
+console.log(sum2(1, 2, 3, 4, 5)); // 15
