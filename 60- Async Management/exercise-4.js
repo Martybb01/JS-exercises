308f793942a9b32bf6910830a574c275e7f9cddb
Martib01 <martybb01@gmail.com>
2022-06-16 17:51:13 +0200

    Promise.all API

---
+++ 60- Async Management/exercise-4.js
@@ -0,0 +1,68 @@
+const persons = [
+  {
+    id: 1,
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
+  },
+  {
+    id: 2,
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
+  },
+  {
+    id: 3,
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: "CEO",
+  },
+  {
+    id: 2,
+    jobTitle: "Project Manager",
+  },
+  {
+    id: 3,
+    jobTitle: "Developer",
+  },
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find((person) => person.id === id);
+      if (person) {
+        return resolve(person);
+      }
+      return reject(`Person with id: ${id} doesn't exist`);
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const job = jobs.find((job) => job.id === id);
+      if (job) {
+        return resolve(job);
+      }
+      return reject(`Job with id: ${id} doesn't exist`);
+    });
+  });
+}
+
+Promise.all([fetchPersonById(2), fetchJobById(2)])
+  .then(([person, job]) => {
+    console.log(person);
+    console.log(job);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
